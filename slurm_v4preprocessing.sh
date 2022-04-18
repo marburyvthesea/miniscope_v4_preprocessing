a3b88d95e9ae2402cdcdd880981560ba46858516
chief <chief>
2022-04-18 17:39:42 -0400

    adding filtering module

---
+++ slurm_v4preprocessing.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH -A p30771
+#SBATCH -p normal
+#SBATCH -t 12:00:00
+#SBATCH -o /home/jma819/caiman_quest/demos/notebooks/caiman_analysis/logfiles/slurm.%x-%j.out # STDOUT
+#SBATCH --job-name="slurm_normcorr"
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH --mem=20G
+
+module purge all
+cd ~
+#add project directory to PATH
+export PATH=$PATH/projects/p30771/
+
+#load modules to use
+module load python/anaconda3.6 
+
+#need to cd to load conda environment
+
+source activate v4_preprocessing
+
+#need to cd to module directory
+
+cd /home/jma819/miniscope/miniscope_v4_preprocessing
+
+#get inputs from command line and run 
+
+INPUT_dataDir=$1
+INPUT_dataFilePrefix=$2
+INPUT_startingFileNum=$3
+#framesPerFile=1000
+
+
+python v4PreProcessingScript $INPUT_dataDir $INPUT_dataFilePrefix $INPUT_startingFileNum
+
+echo "finished preprocessing"
